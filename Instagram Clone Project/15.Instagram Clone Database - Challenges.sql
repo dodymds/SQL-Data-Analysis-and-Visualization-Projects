51824ffcedeb17adbe15ef492094578da1466bc2
dodymds <dodymds@gmail.com>
2022-03-11 22:57:49 +0700

    add query for data analysis

--- Instagram Clone Project/15.Instagram Clone Database - Challenges.sql
+++ Instagram Clone Project/15.Instagram Clone Database - Challenges.sql
@@ -1,44 +1,99 @@
 /*We want to reward our users who have been around the longest.  
-Find the 5 oldest users.*/
-SELECT * FROM users
-ORDER BY created_at
-LIMIT 5;
+Find the 10 oldest users.*/
+SELECT
+  *
+FROM
+  users
+ORDER BY
+  created_at
+LIMIT
+  10;
 
+/*We want to know our users who have been around the shortest.  
+Find the 5 youngest users.*/
+SELECT
+  *
+FROM
+  users
+ORDER BY
+  created_at desc
+LIMIT
+  5;
 
 /*What day of the week do most users register on?
-We need to figure out when to schedule an ad campgain*/
-SELECT date_format(created_at,'%W') AS 'day of the week', COUNT(*) AS 'total registration'
-FROM users
-GROUP BY 1
-ORDER BY 2 DESC;
+We need to figure out when to schedule an ad campaign*/
+SELECT
+  date_format(created_at, '%W') AS 'day of the week',
+  COUNT(*) AS 'total registration'
+FROM
+  users
+GROUP BY
+  1
+ORDER BY
+  2 DESC;
 
 /*version 2*/
-SELECT 
-    DAYNAME(created_at) AS day,
-    COUNT(*) AS total
-FROM users
-GROUP BY day
-ORDER BY total DESC
-LIMIT 2;
+SELECT
+  DAYNAME(created_at) AS day,
+  COUNT(*) AS total
+FROM
+  users
+GROUP BY
+  day
+ORDER BY
+  total DESC
+LIMIT
+  2;
 
+/*What time of the day do most users register on?
+We need to figure out when to schedule an ad campaign*/
+SELECT
+  date_format(created_at, '%H') AS 'time of the day',
+  COUNT(*) AS 'total registration'
+FROM
+  users
+GROUP BY
+  1
+ORDER BY
+  2 DESC;
 
 /*We want to target our inactive users with an email campaign.
 Find the users who have never posted a photo*/
-SELECT username
-FROM users
-LEFT JOIN photos ON users.id = photos.user_id
-WHERE photos.id IS NULL;
+SELECT
+  username
+FROM
+  users
+  LEFT JOIN photos ON users.id = photos.user_id
+WHERE
+  photos.id IS NULL;
 
+/*We want to target our inactive users with an email campaign.
+Find the users who have never posted a comments*/
+SELECT
+  username
+FROM
+  users
+  LEFT JOIN comments ON users.id = comments.user_id
+WHERE
+  comments.id IS NULL;
 
 /*We're running a new contest to see who can get the most likes on a single photo.
 WHO WON??!!*/
-SELECT users.username,photos.id,photos.image_url,COUNT(*) AS Total_Likes
-FROM likes
-JOIN photos ON photos.id = likes.photo_id
-JOIN users ON users.id = likes.user_id
-GROUP BY photos.id
-ORDER BY Total_Likes DESC
-LIMIT 1;
+SELECT
+  users.username,
+  photos.id,
+  photos.image_url,
+  COUNT(*) AS Total_Likes
+FROM
+  likes
+  JOIN photos ON photos.id = likes.photo_id
+  JOIN users ON users.id = likes.user_id
+GROUP BY
+  photos.id
+ORDER BY
+  Total_Likes DESC
+LIMIT
+  1;
 
 /*version 2*/
 SELECT 
